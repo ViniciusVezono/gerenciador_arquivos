4e1de24500d09c2ad83b40f2ce2f12dfe019d639
ViniciusVezono <vivivezono@gmail.com>
2026-06-25 19:31:25 -0300

    fix(env-py): added import for Image and fix tables

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -9,6 +9,7 @@ import os
 import sys
 from app.core.config import settings
 from app.db.database import Base
+from app.models.image import Image 
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
